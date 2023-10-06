2ac1924d27ddb415a6cac6c8952d8da1b26941f1
cjbarrera <54445611+cjbarrera@users.noreply.github.com>
2023-10-06 01:05:33 -0600

    Update main.js

--- 3ra_pre_entrega/main.js
+++ 3ra_pre_entrega/main.js
@@ -1,69 +1,157 @@
-/*
-const tareas =[]
+const fecha = document.querySelector('#fecha')
+const lista = document.querySelector('#lista')
+const elemento = document.querySelector('#elemento')
+const input = document.querySelector('#input')
+const botonEnter = document.querySelector('#boton-enter')
+const check = 'fa-check-circle'
+const uncheck = 'fa-circle'
+const lineThrough = 'line-through'
+let LIST = []
 
-const ingresarTarea = () => {
+let id
 
-let agregar = confirm("¿Quieres agregar una tarea para esta semana?")
 
-while(agregar){
-    const accionTarea = prompt("Que tarea quieres agregar")
-    const fechaRealizacion = prompt("Fecha de realizacion (formato:dd/mm/aa hh:mm)")
-    const fechaLimite = prompt("Fecha de limite (formato:dd/mm/aa hh:mm)")
-    const tipoPriodidad = prompt ("Que prioridad tiene (alta, media o baja)")
-    const confirmacionTarea = confirm("Confirmas tu tarea agendada: " + accionTarea + " el día " + fechaRealizacion + " a más tardar " + fechaLimite + " con prioridad " + tipoPriodidad)
-    if (confirmacionTarea){
-        alert( "Se agendo " + accionTarea + " en la fecha: " + fechaRealizacion)
-        const tarea ={
-            id: Math.round(Math.random() * 1000000000000000),
-            fecha: new Date().toLocaleString(),
-            accion:accionTarea,
-            fechaRealizacion:fechaRealizacion,
-            fechaLimite:fechaLimite,
-            tipoPriodidad:tipoPriodidad,
-            estado:"pendiente"
-        }
-        tareas.push(tarea)
-        console.log(tareas)
-    }
-    else {
-        alert("No se guardo la tarea")
-    }
 
-    agregar = confirm("¿Quieres agregar otra tarea para esta semana?")
+//fecha actualizada auto
+
+const FECHA = new Date ()
+fecha.innerHTML = FECHA.toLocaleDateString('es-MX',{weekday: 'long', month: 'short', day:'numeric'})
+
+
+
+
+
+
+
+// funcion de agregarTarea 
+
+function agregarTarea(tarea, fecha, prioridad, id, realizado, eliminado) {
+    if(eliminado) {return}
+
+    const REALIZADO = realizado ? check : uncheck
+
+    const LINE = realizado ? lineThrough : '' 
+    const elemento = `
+        <div class="white">
+            <i class="far ${REALIZADO}" data="realizado" id="${id}"></i>
+            <p class="text ${LINE}">${tarea}</p>
+            <p class="fecha">${fecha}</p>
+            <p class="prioridad">${prioridad}</p>
+            <i class="fas fa-trash de" data="eliminado" id="${id}"></i>
+        </div>
+    `
+    lista.insertAdjacentHTML("beforeend", elemento);
 }
 
+
+
+
+
+
+
+// funcion de tareaRealizada 
+
+function tareaRealizada(element) {
+    element.classList.toggle(check)
+    element.classList.toggle(uncheck)
+    element.parentNode.querySelector('.text').classList.toggle(lineThrough)
+    LIST[element.id].realizado = LIST[element.id].realizado ?false :true
 }
 
-ingresarTarea()
-*/
+function tareaEliminada(element){
+    element.parentNode.parentNode.removeChild(element.parentNode)
+    LIST[element.id].eliminado = true
+    console.log(LIST)
+}
 
 
 
-const tareas = JSON.parse( localStorage.getItem("tareas")) || []
 
-const ingresarTarea = (id,fecha, tarea, fechaRealizacion, fechaLimite, tipoPriodidad, estado) => {
-    const tarea ={
-        id: Math.round(Math.random() * 1000000000000000),
-        fecha: new Date().toLocaleString(),
-        tarea,
-        fechaRealizacion,
-        fechaLimite,
-        tipoPriodidad,
-        estado:[]
+
+
+// crear un evento para enter y habilitar el botón 
+
+botonEnter.addEventListener('click', () => {
+    const tarea = input.value;
+    const fechaTarea = document.querySelector('#fechaTarea').value;
+    const prioridad = document.querySelector('#prioridad').value;
+
+    if (tarea) {
+        agregarTarea(tarea, fechaTarea, prioridad, id, false, false)
+
+        // localStorage para almacenar la tarea, fecha, prioridad, etc.
+        LIST.push({
+            nombre: tarea,
+            fecha: fechaTarea,
+            prioridad: prioridad,
+            id: id,
+            realizado: false,
+            eliminado: false
+        })
+
+        localStorage.setItem('TODO', JSON.stringify(LIST))
+        id++
+        input.value = ''
+        document.querySelector('#fechaTarea').value = ''
+        document.querySelector('#prioridad').value = 'baja'
     }
-    tareas.push(tareas)
-    localStorage.setItem("tareas",JSON.stringify(tareas))
-
-}
-
-const formIngresarTarea = document.getElementById("formIngresarTarea")
-formIngresarTarea.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    const datos = e.target.children
-    ingresarTarea(datos["id"].value,datos["fecha"].value,datos["tarea"].value,datos["fechaRealizacion"].value,datos["fechaLimite"].value,)
-
 })
 
 
 
 
+document.addEventListener('keyup', function (event) {
+    if (event.key=='Enter'){
+        const tarea = input.value
+        if(tarea) {
+            agregarTarea(tarea,id,false,false)
+        LIST.push({
+            nombre : tarea,
+            id : id,
+            realizado : false,
+            eliminado : false
+        })
+        localStorage.setItem('TODO',JSON.stringify(LIST))
+     
+        input.value = ''
+        id++
+        }
+    }
+    
+})
+
+
+lista.addEventListener('click',function(event){
+    const element = event.target 
+    const elementData = element.attributes.data.value
+    console.log(elementData)
+    
+    if(elementData == 'realizado') {
+        tareaRealizada(element)
+    }
+    else if(elementData == 'eliminado') {
+        tareaEliminada(element)
+        console.log("elimnado")
+    }
+    localStorage.setItem('TODO',JSON.stringify(LIST))
+})
+
+
+
+
+let data = localStorage.getItem('TODO')
+if(data){
+    LIST = JSON.parse(data)
+    id = LIST.length
+    cargarLista(LIST)
+}else {
+    LIST = []
+    id = 0
+}
+
+
+function cargarLista(array) {
+    array.forEach(function(item){
+        agregarTarea(item.nombre, item.fecha, item.prioridad, item.id, item.realizado, item.eliminado)
+    })
+}
